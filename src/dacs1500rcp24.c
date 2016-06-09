52f98423df717c565909f45112aee3621d9d6765
HATTORI Kohei <hattori@team-lab.com>
2016-06-09 13:16:57 +0900

    change period

--- src/dacs1500rcp24.c
+++ src/dacs1500rcp24.c
@@ -5,7 +5,7 @@
 #define PWM_OPCODE 'Q'
 #define PWM_DEVICE_ID '0'
 #define PWM_CLOCK_PERIOD 1  // 8us 4us 2us 1us 0.5us 0.25us 0.125us 0.0625usの中から指定する
-#define PWM_PALSE_PERIOD 1000  // us単位 パルス周期/クロック周期が4096を超えないように指定する
+#define PWM_PALSE_PERIOD 20000  // us単位 パルス周期/クロック周期が4096を超えないように指定する
 
 void initDio();
 void exitDio();
