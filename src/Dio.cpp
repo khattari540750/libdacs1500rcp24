b569a2423455d31ae6fd1dd1c06797af5583ec0e
HATTORI Kohei <hattori@team-lab.com>
2016-06-09 18:25:49 +0900

    Dio add

--- src/Dio.cpp
+++ src/Dio.cpp
@@ -30,7 +30,7 @@ void Dio::close() {
 
 
 char* Dio::toHex(unsigned int x) {
-  staic char result[6];
+  static char result[6];
   for (int i = 0; i < 6; i++) {
     if (x % 16 >= 10) {
       result[5 - i] = (char)((int)'A' + (x % 16 - 10));
