577ded09890fcd70a4299a85282e8646035b9baf
HATTORI Kohei <hattori@team-lab.com>
2016-06-10 15:48:18 +0900

    Dio add

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,19 @@
-//#include "dacs1500rcp24.h"
+#include "dacs1500rcp24.hpp"
+
+int main(void)
+{
+	Dacs1500rcp24 dio;
+
+	dio.open();
+
+	dio.close();
+	
+	return 0;
+}
+
+
+
+/*
 #include "Dio.hpp"
 #include <unistd.h>
 #include <iostream>
@@ -53,99 +68,5 @@ int main(void)
 	dio.close();
 
 	return 0;
-
-
-
-
-
-
-	/*
-	Dio dio;
-	char *command;
-	int values[2];
-
-	dio.open();
-
-	dio.ledOn();
-	sleep(2);
-	dio.ledOff();
-	sleep(1);
-
-	// 初期化
-	command = dio.getPWMInitializeCommand();
-	std::cout << command << std::endl;
-	dio.sendCommandToDio(command);
-
-	//初期位置
-	values[0] = 1450;
-	values[1] = 1450;
-	dio.changePWMPalse(values);
-	//dio.changePWMPalse(0, 1450);
-
-	// start
-	command = dio.getPWMStartCommand();
-	std::cout << command << std::endl;
-	dio.sendCommandToDio(command);
-
-	// 位置変更
-	for (int i=0; i<10; i++) {
-    sleep(1);
-    if(i%2==0) {
-			values[0] = 2400;
-			values[1] = 2400;
-			dio.changePWMPalse(values);
-			//dio.changePWMPalse(0,2400);
-		}
-    else {
-			values[0] = 500;
-			values[1] = 500;
-			dio.changePWMPalse(values);
-			//dio.changePWMPalse(0,500);
-		}
-	}
-
-
-	dio.close();
-
-	return 0;
-	*/
-
-
-
-
-
-
-
-	/*
-	char *code;
-
-	initDio();
-
-	// 初期化
-	code = getPWMInitializeCode();
-	printf("%s\n", code);
-	sendCommandCodeToDio(code);
-
-	// PWMパルス幅の初期位置設定
-	code = getPWMPalseChangeUsecCode(0,1450);
-	printf("%s\n", code);
-	sendCommandCodeToDio(code);
-
-	// PWM出力開始
-	code = getPWMStartCode();
-	printf("%s\n", code);
-	sendCommandCodeToDio(code);
-
-  // PWMパルス幅の変更 繰り返し
-  int i;
-  for (i=0; i<10; i++) {
-    sleep(1);
-    if(i%2==0) code = getPWMPalseChangeUsecCode(0,2400);
-    else code = getPWMPalseChangeUsecCode(0,500);
-  	printf("%s\n", code);
-  	sendCommandCodeToDio(code);
-  }
-
-	exitDio();
-	*/
 }
+*/
